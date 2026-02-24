84e52c37f997cf3091fe9e2227672ee634e599b8
pc <pc@192.168.1.9>
2026-02-24 18:34:18 +0530

    First commit

--- AutomationJavaSeleniumproject/src/test/java/com/seleniumdemo/TC01_FirstDemo.java
+++ AutomationJavaSeleniumproject/src/test/java/com/seleniumdemo/TC01_FirstDemo.java
@@ -7,7 +7,7 @@ public class TC01_FirstDemo {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.makemytrip.com/");
+		driver.get("https://google.com/");
 		System.out.println("page Title:::  "+ driver.getTitle());
 		driver.quit();
 
