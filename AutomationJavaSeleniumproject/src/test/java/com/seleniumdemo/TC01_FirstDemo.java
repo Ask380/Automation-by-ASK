da7fe80659aa6b086b7641ccefbda813ed82c6da
pc <pc@192.168.1.9>
2026-02-24 18:23:40 +0530

    First Commit

---
+++ AutomationJavaSeleniumproject/src/test/java/com/seleniumdemo/TC01_FirstDemo.java
@@ -0,0 +1,16 @@
+package com.seleniumdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC01_FirstDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.makemytrip.com/");
+		System.out.println("page Title:::  "+ driver.getTitle());
+		driver.quit();
+
+	}
+
+}
